ae6ecb66a9713e9e9e2244e751aaee0a26b10a11
Alexander <aexro2221@gmail.com>
2026-05-14 11:27:13 -0400

    added test hardocded names for browse heroes screen

--- OverStatsFinal/OverStatsFinal/Controllers/HeroListViewController.swift
+++ OverStatsFinal/OverStatsFinal/Controllers/HeroListViewController.swift
@@ -4,17 +4,48 @@
 //
 //  Created by user933335 on 5/13/26.
 //
-
+//UIKit as term specs
 import UIKit
 
+
+//Display of hero names to have practice screen before API implementation
 class HeroListViewController : UIViewController
 {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let heroes = [ "Tracer", "Reinhardt", "Mercy", "Genji", "D.Va"]
+    
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        //Screen will display character in the game known as heroes
         title = "Heroes"
-        view.backgroundColor = .systemBackground
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HeroListViewController: UITableViewDelegate
+{
+    
+}
+
+//Data seperation as hero info enters
+extension HeroListViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return heroes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "HeroCell")
+        cell.textLabel?.text = heroes [indexPath.row]
+        
+        return cell
     }
 }
